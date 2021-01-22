7b8687d3868ed6796e3fee1cda94e4aef1b3ecba
Gonçalo Almeida <32540757+xaleza@users.noreply.github.com>
2021-01-21 23:11:46 -0100

    Data layer for the Weather feature done; closes #2

--- azores_weather/lib/features/weather/data/datasources/spot_remote_data_source.dart
+++ azores_weather/lib/features/weather/data/datasources/spot_remote_data_source.dart
@@ -1,8 +1,34 @@
+import 'dart:convert';
+
+import 'package:azores_weather/core/error/exception.dart';
 import 'package:azores_weather/features/weather/data/models/spot_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+const API_KEY = "0c07c51b5984e4bb4745cd6d4aaa8a42";
 
 abstract class SpotRemoteDataSource {
-  /// Calls the api.openweathermap.org/data/2.5/weather?id={spot id}&appid={API key} endpoint.
+  /// Calls the api.openweathermap.org/data/2.5/weather?id={spot id}&appid={API key}&units=metric endpoint.
   ///
   /// Throws a [ServerException] for all error codes.
   Future<SpotModel> getFreshWeatherForSpot(int spotId);
+}
+
+class SpotRemoteDataSourceImpl implements SpotRemoteDataSource {
+  final http.Client client;
+
+  SpotRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<SpotModel> getFreshWeatherForSpot(int spotId) async {
+    final response = await client.get(
+      'api.openweathermap.org/data/2.5/weather?id=$spotId&appid=$API_KEY&units=metric',
+      headers: {'Content-Type': 'application/json'},
+    );
+    if (response.statusCode == 200) {
+      return SpotModel.fromJson(json.decode(response.body));
+    } else {
+      throw ServerException();
+    }
+  }
 }
---
+++ azores_weather/test/data/datasources/spot_remote_data_source_test.dart
@@ -0,0 +1,72 @@
+import 'dart:convert';
+
+import 'package:azores_weather/core/error/exception.dart';
+import 'package:azores_weather/core/network/network_info.dart';
+import 'package:azores_weather/features/weather/data/datasources/spot_remote_data_source.dart';
+import 'package:azores_weather/features/weather/data/models/spot_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:http/http.dart' as http;
+
+import '../../fixtures/fixture_reader.dart';
+
+class MockHttpClient extends Mock implements http.Client {}
+
+void main() {
+  SpotRemoteDataSource dataSource;
+  MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    dataSource = SpotRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  final tSpotId = 3372783;
+
+  test(
+    'should preform a GET request on a URL with number being the endpoint and with application/json header',
+    () {
+      //arrange
+      when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => http.Response(fixture('weather.json'), 200),
+      );
+      // act
+      dataSource.getFreshWeatherForSpot(tSpotId);
+      // assert
+      verify(mockHttpClient.get(
+        'api.openweathermap.org/data/2.5/weather?id=$tSpotId&appid=$API_KEY&units=metric',
+        headers: {'Content-Type': 'application/json'},
+      ));
+    },
+  );
+
+  final tSpotModel = SpotModel.fromJson(json.decode(fixture('weather.json')));
+
+  test(
+    'should return Spot when the response code is 200 (success)',
+    () async {
+      // arrange
+      when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => http.Response(fixture('weather.json'), 200),
+      );
+      // act
+      final result = await dataSource.getFreshWeatherForSpot(tSpotId);
+      // assert
+      expect(result, equals(tSpotModel));
+    },
+  );
+
+  test(
+    'should throw a ServerException when the response code is 404 or other',
+    () async {
+      // arrange
+      when(mockHttpClient.get(any, headers: anyNamed('headers'))).thenAnswer(
+        (_) async => http.Response('Something went wrong', 404),
+      );
+      // act
+      final call = dataSource.getFreshWeatherForSpot;
+      // assert
+      expect(() => call(tSpotId), throwsA(isA<ServerException>()));
+    },
+  );
+}
